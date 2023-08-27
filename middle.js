82be27f2bb76954b3d6f73db346b4ad3b1e5211a
Hisban Shiraz <hisbanbardai@gmail.com>
2023-08-27 15:52:57 -0600

    Modified middle function with mentor's feedback

--- middle.js
+++ middle.js
@@ -18,22 +18,38 @@ const eqArrays = function(arr1, arr2){
     return true;
   }
 }
-
+//Mentor's feedback
 const middle = function(array) {
-  let finalArray = [];
-  if (array.length < 3){
-    return finalArray;
-  } else {
-    if (array.length % 2 !== 0){
-      finalArray.push(array[Number.parseInt(array.length / 2)]);
-      return finalArray;
-    } else {
-      finalArray.push(array[(array.length / 2) - 1]);
-      finalArray.push(array[array.length / 2]);
-      return finalArray;
-    }
+  const length = array.length;
+  if(length <= 2){
+    return [];
+  }
+  const middleIndex = Math.floor(length / 2);
+  if (length % 2 === 0 && length > 3){
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+
+  if (length > 2){
+    return [array[middleIndex]];
   }
 }
+
+//My code
+//   const middle = function(array) {
+//   let finalArray = [];
+//   if (array.length < 3){
+//     return finalArray;
+//   } else {
+//     if (array.length % 2 !== 0){
+//       finalArray.push(array[Number.parseInt(array.length / 2)]);
+//       return finalArray;
+//     } else {
+//       finalArray.push(array[(array.length / 2) - 1]);
+//       finalArray.push(array[array.length / 2]);
+//       return finalArray;
+//     }
+//   }
+// }
 
 assertArraysEqual(middle([1, 2, 3]),[2]);
 assertArraysEqual(middle([1, 2]),[]);
