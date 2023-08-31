f341e2dfaa115403e0ae8958d0dd1be76cc3d901
Hisban Shiraz <hisbanbardai@gmail.com>
2023-08-31 13:19:26 -0600

    Created a assertion function for objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,31 @@
+const eqObjects = (object1, object2) => {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (const key1 of Object.keys(object1)) {
+      if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
+        if (!eqArrays(object1[key1], object2[key1])) {
+          return false;
+        }
+      } else {
+        if (object1[key1] !== object2[key1]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (!eqObjects(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)}  !==  ${inspect(expected)}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)}  ===  ${inspect(expected)}`);
+  }
+};
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject);
