955a65992897e0c1dade14f808d3ad98b96a233d
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-02 14:43:28 -0600

    Implemented findKey method using callback function

---
+++ findKey.js
@@ -0,0 +1,50 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  //Using for of loop
+  // for (const key of Object.keys(object)) {
+  //   if (callback(object[key])){
+  //     return key;
+  //   }
+  // }
+
+  //Using for in loop
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      if (callback(object[key])) {
+        return key;
+      }
+    }
+  }
+  return undefined;
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5), undefined);
