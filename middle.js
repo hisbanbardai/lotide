8e3287247a2b19d6d996da5fcd4e42deef51947a
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-15 08:17:10 -0600

    Refactored middle function

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const assertArraysEqual = function(actual, expected){
-  if (!eqArrays(actual, expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !==  ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  }
-};
 
-const eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length){
-    return false;
-  } else{
-    for (let i = 0; i < arr1.length; i++){
-      if (arr1[i] !== arr2[i]){
-        return false;
-      }
-    }
-    return true;
-  }
-}
 //Mentor's feedback
 const middle = function(array) {
   const length = array.length;
@@ -51,6 +32,4 @@ const middle = function(array) {
 //   }
 // }
 
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]),[4,5]);
+module.exports = middle;
